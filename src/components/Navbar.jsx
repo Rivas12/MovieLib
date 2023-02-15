4506d984ae3e5f6f709c6c140e74ff8312c423ba
Rivaldo <rivaldosilveira13@gmail.com>
2023-02-15 01:26:22 -0300

    Estrutura de dados React criada

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav id="navbar">
+                <h2>
+                    <Link to="/"><BiCameraMovie /> Movies Lib</Link>
+                </h2>
+                <form>
+                    <input type="text" placeholder="Procure um filme" />
+                    <button type="submit"><BiSearchAlt2 /></button>
+                </form>
+            </nav>
+        </div>
+    );
+}
+export default Navbar
