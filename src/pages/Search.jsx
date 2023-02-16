1ec19650c19b5cb1f38e4d67665b40e6ea606f9d
Rivaldo <rivaldosilveira13@gmail.com>
2023-02-16 18:48:34 -0300

    sistema de busca integrado

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+
+import "./MovieGrid.css";
+
+const searchURL = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
+
+
 const Search = () => {
+
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get("q");
+
+    const [Movies, setMovie] = useState([]);
+
+    const getSearchedMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setMovie(data.results);
+    };
+
+    useEffect(() => {
+        const searchWithQueryUrl = `${searchURL}?${apiKey}&query=${query}`;
+
+        getSearchedMovies(searchWithQueryUrl);
+    }, [query]);
+
     return (
-        <div>Search</div>
+        <div className="container">
+            <h2 className="title">Resultados para: <span className="query-text">{query}</span></h2>
+            <div className="moviesContainer">
+                {Movies.length > 0 && Movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
+        </div>
     );
 };
 export default Search;
