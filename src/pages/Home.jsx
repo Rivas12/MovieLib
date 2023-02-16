1ae6d35b85d11bdc2772a0be231dc9bfa4c61e09
Rivaldo <rivaldosilveira13@gmail.com>
2023-02-16 01:06:55 -0300

    criacao de rotas, pagina Home configurada e API funcionando

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,35 @@
+import { useState, useEffect } from 'react';
+
+import MovieCard from '../components/MovieCard';
+
+import "./MovieGrid.css";
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
 const Home = () => {
+    const [TopMovies, setTopMovies] = useState([]);
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setTopMovies(data.results);
+    };
+
+    useEffect(() => {
+        const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+        getTopRatedMovies(topRatedUrl)
+    }, []);
+
     return (
-        <div></div>
+        <div className="container">
+            <h2 className="title">Melhores filmes</h2>
+            <div className="moviesContainer">
+                {TopMovies.length > 0 && TopMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
+        </div>
     );
 };
 export default Home;
